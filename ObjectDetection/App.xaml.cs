201d1a3b800f77901059e79e47583eb390ff7f48
Niels Schuler <niels.schuler@web.de>
2024-06-20 13:57:21 +0200

    Projektdateien hinzufügen.

---
+++ ObjectDetection/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ObjectDetection
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ObjectDetection/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ObjectDetection
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
