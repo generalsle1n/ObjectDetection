f55df010daab51e58f2156436c06c2b81850b6f9
Niels Schuler <niels.schuler@web.de>
2024-06-20 15:48:25 +0200

    Working Picture load

--- ObjectDetection/MainPage.xaml.cs
+++ ObjectDetection/MainPage.xaml.cs
@@ -1,24 +1,39 @@
-﻿namespace ObjectDetection
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace ObjectDetection
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private readonly PickOptions _pickOptions = new PickOptions()
+        {
+            PickerTitle = AppInfo.Name,
+            FileTypes = FilePickerFileType.Images
+        };
+
+        private const string _splitContentType = "/";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnLoadClickAsync(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            //File Dialog
+            FileResult Result = await FilePicker.PickAsync(_pickOptions);
+            //ReadFile into Matrix
+            Mat Matrix = CvInvoke.Imread(Result.FullPath, Emgu.CV.CvEnum.ImreadModes.Color);
+            //Convert Matrix into Source FileType
+            byte[] ResultData = CvInvoke.Imencode($".{(Result.ContentType.Split(_splitContentType))[1]}", Matrix);
+            //Create Memory Stream for ImageSource
+            MemoryStream MemoryStream = new MemoryStream(ResultData);
+            //Create the Source to be loadable into Maui
+            ImageSource Source = ImageSource.FromStream(() => MemoryStream);
+            //Load the source
+            ImageBox.Source = Source;
+           
         }
     }
 
